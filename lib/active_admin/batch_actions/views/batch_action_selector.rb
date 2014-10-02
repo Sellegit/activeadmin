ee374a4dfa89c00b1f90b59cb9530a9f55c9b746
Rocky Duan <dementrock@gmail.com>
2014-10-01 17:30:32 -0700

    update

--- lib/active_admin/batch_actions/views/batch_action_selector.rb
+++ lib/active_admin/batch_actions/views/batch_action_selector.rb
@@ -24,8 +24,7 @@ module ActiveAdmin
 
       def build_drop_down
         dropdown_menu I18n.t("active_admin.batch_actions.button_label"),
-                      class: "batch_actions_selector dropdown_menu",
-                      button: { class: "disabled" } do
+                      class: "batch_actions_selector dropdown_menu" do
           batch_actions_to_display.each do |batch_action|
             confirmation_text = render_or_call_method_or_proc_on(self, batch_action.confirm)
 
